cb6046d73b3eb49a0e2f4d922402bc98d458e62e
unknown <gildasilvestrelopez09@gmail.com>
2021-04-19 21:24:57 -0400

    Create Spring boot project

---
+++ GetItProject/src/main/java/comgetit/GetItProjectApplication.java
@@ -0,0 +1,13 @@
+package comgetit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetItProjectApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(GetItProjectApplication.class, args);
+	}
+
+}
---
+++ GetItProject/src/test/java/comgetit/GetItProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package comgetit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetItProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
