47ec8500827ae47555942f91631f5347dd7c9a0a
Gilda Silvestre Lopez <gilda.silvestre@fundacion-jala.org>
2021-05-06 20:19:28 -0700

    Fix indentation

--- GetItProject/src/main/java/comgetit/GetItProjectApplication.java
+++ GetItProject/src/main/java/comgetit/GetItProjectApplication.java
@@ -11,7 +11,7 @@ public class GetItProjectApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(GetItProjectApplication.class, args);
-    }
+	}
 
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
